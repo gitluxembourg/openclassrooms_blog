6f7ab81a4d8efb78b46315dbd3cebb39f8a1a216
patrick pierra <patrick.pierra@pt.lu>
2018-12-27 17:56:06 +0100

    blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post} from './model/post';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'blog';
+export class AppComponent implements OnInit {
+  title = 'POSTS';
+  posts = new Array<Post>();
+
+  constructor() {
+    this.posts.push(new Post('premier post', 'contenu du premier post'));
+    this.posts.push(new Post('deuxieme post', 'contenu du deuxieme post'));
+    this.posts.push(new Post('troisieme post', 'contenu du troisieme post'));
+  }
+  ngOnInit() {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostlistComponent } from './postlist/postlist.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostlistComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
